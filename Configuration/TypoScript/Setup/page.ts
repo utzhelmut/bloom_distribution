0a783c959125b649e84cc71689aa6636911738ec
root <root@precise32.(none)>
2014-09-10 12:45:30 +0000

    first commit

---
+++ Configuration/TypoScript/Setup/page.ts
@@ -0,0 +1,22 @@
+page = PAGE
+#page.config.metaCharset = utf-8
+page{
+    config{
+        metaCharset = utf-8
+        additionalHeaders = Content-Type:text/html;charset=utf-8
+    }
+    includeCSS.bootstrap = EXT:bloom_distribution/Resources/Public/Bootstrap/css/bootstrap.min.css
+    includeCSS.bootstratheme = EXT:bloom_distribution/Resources/Public/Bootstrap/css/bootstrap-theme.min.css
+    includeCSS.bootstrapcustom = EXT:bloom_distribution/Resources/Public/Css/custom.css
+    includeJS.bootstrap = EXT:bloom_distribution/Resources/Public/Bootstrap/js/bootstrap.min.js
+    10 = FLUIDTEMPLATE
+    10{
+        file = EXT:bloom_distribution/Resources/Private/Template/index.html
+        layoutRootPath = EXT:bloom_distribution/Resources/Private/Layouts/
+        partialRootPath = EXT:bloom_distribution/Resources/Private/Partials/
+        variables{
+          
+        }
+    }
+}
+
